93c37fa5bde2ffb587bc112210067b819e5f17c8
Odeta Maslovaite <odetamaslovaite@yahoo.com>
2020-07-09 11:05:30 +0200

    ïn progress

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
 
-app.use(express.json())
+
+
+morgan.token('body', (request, response) =>{
+return JSON.stringify(request.body)
+});
+
+morgan.format('combined',
+ ":method :url :status :res[content-length] - :response-time ms :body");
+
+
+app.use(morgan('tiny', {
+    skip: (request, response) => {
+        return request.method === 'POST';
+    }
+}));
+
+app.use(morgan('combined', {skip: (request, response) => request.method !== 'POST'}));
+
+app.use(express.json());
 
 let persons = [
     {
@@ -26,18 +45,18 @@ let persons = [
         number: "39-23-6423122",
         id: 4
       }
-  ]
+  ];
 
-  app.get('/info', (req, res) => {
-    res.send(`
+  app.get('/info', (request, response) => {
+    response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
 `);
-  })
+  });
   
-  app.get('/api/persons', (req, res) => {
-    res.json(persons)
-  })
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  });
 
   
   app.post('/api/persons', (request, response) => {
@@ -47,29 +66,32 @@ let persons = [
       return response.status(400).json({ 
         error: 'name missing' 
       })
-    }
+    };
     
     if(!addPerson.number) {
       return response.status(400).json({ 
         error: 'number missing' 
       })
-    }
+    };
     
     
     const person = {
       name: addPerson.name,
       number: addPerson.number,
       id: Math.floor(Math.random() * 99999999)
-    }
-    if(addPerson.name === person.name) {
+    };
+
+    const matchingName = persons.find(p =>
+      p.name.toUpperCase() === addPerson.name.toUpperCase());
+    if(matchingName !== undefined) {
       return response.status(400).json({ 
         error: 'name must be unique'  
       })
-    }
+    };
     persons = persons.concat(person)
   
     response.json(person)
-  })
+  });
 
   app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -79,16 +101,16 @@ let persons = [
     } else {
       response.status(404).end()
     }
-  })
+  });
   
   app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
-  })
+  });
   // checking if HTTP requests are working by using Postman
 
 const port = 3001
 app.listen(port)
-console.log(`Server running on port ${port}`)
+console.log(`Server running on port ${port}`);
