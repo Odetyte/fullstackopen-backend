00da02015848fe8907edb67de8d78c32d8265717
Odeta Maslovaite <odetamaslovaite@yahoo.com>
2020-07-11 19:34:14 +0200

    errors fixed

--- index.js
+++ index.js
@@ -27,31 +27,6 @@ app.use(morgan('combined', { skip: (request) => request.method !== 'POST' }))
 
 app.use(express.json())
 
-let persons = [
-  {
-
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1
-  },
-  {
-
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4
-  }
-]
-
 app.get('/info', (request, response) => {
   response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
