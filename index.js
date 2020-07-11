80f2697d4285ff71e4f6721aca61962710094d3d
Odeta Maslovaite <odetamaslovaite@yahoo.com>
2020-07-11 19:59:12 +0200

    errors fixed

--- index.js
+++ index.js
@@ -27,11 +27,38 @@ app.use(morgan('combined', { skip: (request) => request.method !== 'POST' }))
 
 app.use(express.json())
 
-app.get('/info', (request, response) => {
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
+let persons = [
+  {
+
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4
+  }
+]
+
+app.get('/info', (req, res) => {
+  Person.countDocuments({}).then(personsLength => {
+    res.send(`
+    <p>Phonebook has info for ${personsLength} people</p>
     <p>${new Date()}</p>
 `)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
